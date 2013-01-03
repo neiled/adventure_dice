404818d8a4df1f00badfb09d677fa67a8676ae77
Neil Edwards <neil@plasticwater.com>
2013-01-03 11:23:47 -0500

    model the modifier property

--- app/models/dice.rb
+++ app/models/dice.rb
@@ -1,16 +1,18 @@
 class Dice
   attr_accessor :sides
   attr_accessor :result
+  attr_accessor :modifier
 
-  def initialize(sides)
+  def initialize(sides, modifier = 0)
     self.sides = sides
+    self.modifier = modifier
     roll
     
     self
   end
   
   def roll
-    self.result = Random.rand(sides)+1
+    self.result = Random.rand(sides) + 1 + modifier
   end
 
 end
