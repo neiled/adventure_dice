289401361c69401bdbf476c9bfae453fe048b4f0
Neil Edwards <neil@plasticwater.com>
2013-01-03 08:03:09 -0500

    attempt to use teacup

--- app/controllers/dice_results_controller.rb
+++ app/controllers/dice_results_controller.rb
@@ -1,9 +1,17 @@
 class DiceResultsController < UIViewController
+    include BW::KVO
+
+  stylesheet :dice_results
+
   def initWithDice(selected_dice)
     self.initWithNibName(nil, bundle:nil)
     generate_results(selected_dice)
 
     self
+  end
+
+  layout :result do
+    subview(UIButton, :result)
   end
 
   def viewDidLoad
@@ -27,5 +35,9 @@ class DiceResultsController < UIViewController
       30 + 20 * index]
     label.autoresizingMask = UIViewAutoresizingFlexibleBottomMargin | UIViewAutoresizingFlexibleTopMargin
     self.view.addSubview(label)
+
+    observe(dice, :result) do |old_value, new_value|
+      label.text = "d" + dice.sides.to_s + " = " + new_value.to_s
+    end
   end
 end
---
+++ app/styles/results_view.rb
@@ -0,0 +1,5 @@
+Teacup::Stylesheet.new :dice_results do
+  style :result,
+    frame: [[20, 300], [50, 20]],
+    title: "Roll Again"
+end
