ec36db97a0846c556112bbb6b9f54f1ee3e29b33
Neil Edwards <neil@plasticwater.com>
2013-01-08 16:43:09 -0500

    option for hiding save button

--- app/models/results_mixin.rb
+++ app/models/results_mixin.rb
@@ -1,13 +1,13 @@
 module EnableRollResults
 
   #was @selected_dice.values
-  def roll_dice(dice)
+  def roll_dice(dice, show_save_button = true)
     @dice_being_rolled = dice
     @results_controller = DiceResultsController.alloc.initWithDice(@dice_being_rolled)
     bar_button_close = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemDone, target: self, action:"close")
     bar_button_save = UIBarButtonItem.alloc.initWithBarButtonSystemItem(UIBarButtonSystemItemSave, target: self, action:"add_favorite")    
     @results_controller.navigationItem.rightBarButtonItem = bar_button_close
-    @results_controller.navigationItem.leftBarButtonItem = bar_button_save
+    @results_controller.navigationItem.leftBarButtonItem = bar_button_save if show_save_button
     self.presentViewController(
       UINavigationController.alloc.initWithRootViewController(@results_controller),
       animated:true,
