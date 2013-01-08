26dddf607e3c64e515bd5fbf3b96fa93800445d0
Neil Edwards <neil@plasticwater.com>
2013-01-08 13:51:54 -0500

    implement encoding interface

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,9 +1,29 @@
 class Favourite
-  include MotionModel::Model
-  
-  columns     :name => :string
-  has_many    :dice
 
-  #attr_accessor :dice
-  #attr_accessor :name
+  PROPERTIES = [:dice, :name]
+  PROPERTIES.each { |prop|
+    attr_accessor prop
+  }
+
+  def initialize(attributes = {})
+    attributes.each { |key, value|
+      self.send("#{key}=", value) if PROPERTIES.member? key
+    }
+  end  
+  
+  def initWithCoder(decoder)
+    self.init
+    PROPERTIES.each { |prop|
+      value = decoder.decodeObjectForKey(prop.to_s)
+      self.send((prop.to_s + "=").to_s, value) if value
+    }
+    self
+  end
+
+  # called when saving an object to NSUserDefaults
+  def encodeWithCoder(encoder)
+    PROPERTIES.each { |prop|
+      encoder.encodeObject(self.send(prop), forKey: prop.to_s)
+    }
+  end  
 end
