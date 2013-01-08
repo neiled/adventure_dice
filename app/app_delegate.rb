6349c3cf8bb009998cc713af775d6b09ef6efab6
Neil Edwards <neil@plasticwater.com>
2013-01-08 14:13:45 -0500

    remove old persistence methods

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -12,18 +12,18 @@ class AppDelegate
     true
   end
   
-  def favourites
-    App::Persistence['favourites'] = Array.new unless App::Persistence['favourites']
-    Array.new(App::Persistence['favourites'])
-  end
+#  def favourites
+#    App::Persistence['favourites'] = Array.new unless App::Persistence['favourites']
+#    Array.new(App::Persistence['favourites'])
+#  end
 
-  def add_favourite(dice)
-    current = favourites
-    current << dice
-    App::Persistence['favourites'] = current
-  end
+#  def add_favourite(dice)
+#    current = favourites
+#    current << dice
+#    App::Persistence['favourites'] = current
+#  end
 
-  def delete_favourite(index)
-    App::Persistence['favourites'].delete_at(index)
-  end
+#  def delete_favourite(index)
+#    App::Persistence['favourites'].delete_at(index)
+#  end
 end
