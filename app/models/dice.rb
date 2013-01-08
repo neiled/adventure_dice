5e23fabdd80e55648080f1ea8ded51b1eda7edcf
Neil Edwards <neil@plasticwater.com>
2013-01-08 11:39:38 -0500

    move to use motionmodel

--- app/models/dice.rb
+++ app/models/dice.rb
@@ -1,17 +1,9 @@
 class Dice
-  PROPERTIES = [:sides, :result, :modifier]
-  PROPERTIES.each { |prop|
-    attr_accessor prop
-  }
-
-  def initialize(attributes = {})
-    attributes.each { |key, value|
-      self.send("#{key}=", value) if PROPERTIES.member? key
-    }
-    modifier = 0 unless modifier
-    roll
-    self
-  end
+  include MotionModel::Model
+  
+  columns :sides => :int,
+          :result => :int,
+          :modifier => {:type => :int, :default => 0}
 
   def roll
     self.result = Random.rand(sides) + 1 + modifier
@@ -29,22 +21,6 @@ class Dice
     else
       ""
     end
-  end
-
-  def initWithCoder(decoder)
-    self.init
-    PROPERTIES.each { |prop|
-      value = decoder.decodeObjectForKey(prop.to_s)
-      self.send((prop.to_s + "=").to_s, value) if value
-    }
-    self
-  end
-
-  # called when saving an object to NSUserDefaults
-  def encodeWithCoder(encoder)
-    PROPERTIES.each { |prop|
-      encoder.encodeObject(self.send(prop), forKey: prop.to_s)
-    }
   end
 
 end
