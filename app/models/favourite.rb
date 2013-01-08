c297530b4998966f4923d964260d4ff1f6582170
Neil Edwards <neil@plasticwater.com>
2013-01-08 11:25:04 -0500

    moving to motionmodel for peristence

--- app/models/favourite.rb
+++ app/models/favourite.rb
@@ -1,4 +1,9 @@
 class Favourite
-  attr_accessor :dice
-  attr_accessor :name
+  include MotionModel::Model
+  
+  columns     :name => :string
+  has_many    :dice
+
+  #attr_accessor :dice
+  #attr_accessor :name
 end
