1a99e743bfa6fe6d412164eaf917954c84a1dc73
Neil Edwards <neil@plasticwater.com>
2013-01-11 12:35:13 -0500

    initialise form and attach testflight feedback

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,8 +1,37 @@
-class SettingsController < UIViewController
+class SettingsController < Formotion::FormController
+  
+  PERSIST_AS = :settings
+  
+  SETTINGS_HASH = {
+      title: "Adventure Dice",
+      persist_as: PERSIST_AS,
+      sections: [{
+        title: "Settings",
+        rows: [{
+          title: "Shake to Re-Roll",
+          type: :swtich,
+          key: :shake,
+        }, {
+          title: "Animate Results",
+          type: :switch,
+          key: :animate,
+        }]
+      }, {
+        title: "Help",
+        rows: [{
+          title: "Give Feedback",
+          type: "submit"]
+    }
   
   def initWithNibName(name, bundle: bundle)
     super
     self.tabBarItem = UITabBarItem.alloc.initWithTitle("Settings", image: UIImage.imageNamed("20-gear-2"), tag: 3)
-    self
+    f = Formotion::Form.persist(SETTINGS_HASH)
+    initWithForm(f)    
   end
+  
+  def submit
+    TestFlight.openFeedbackView
+  end
+  
 end
